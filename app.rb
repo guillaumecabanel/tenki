f328db76629deec5f6c0ebdf96a605ac99ddf4f4
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2020-12-11 18:40:42 +0100

    add city list

--- app.rb
+++ app.rb
@@ -2,10 +2,21 @@ require 'sinatra'
 require_relative 'helpers.rb'
 require_relative 'lib/forecast.rb'
 
+enable :sessions
+
+get '/' do
+  redirect(to("/#{session[:last_visited_city]}")) if session[:last_visited_city] && params[:reset_last_visited_city] != 'true'
+
+  @cities = Forecast::CITIES
+  slim :index
+end
+
 get '/:city' do
+  session[:last_visited_city] = params[:city]
+
   @icons    = Forecast::ICONS
   @today    = Forecast.today(params[:city])
   @now_temp = Forecast.now_temp(params[:city])
 
-  slim :index
+  slim :show
 end
